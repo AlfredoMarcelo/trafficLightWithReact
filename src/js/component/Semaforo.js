2e059ed6693df3c77f5dc144e3c486d40d77f5c9
AlfredoMarcelo <alfredo.supanta.26@gmail.com>
2021-11-05 02:07:22 +0000

    Avance luces y fondo

---
+++ src/js/component/Semaforo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Semaforo() {
+	return (
+		<div className="semaforo">
+			<div className="luz roja"></div>
+			<div className="luz amarilla"></div>
+			<div className="luz verde"></div>
+		</div>
+	);
+}
